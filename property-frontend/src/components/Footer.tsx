e60d26e6ce876769dc6d950c6582663f602d415a
roobankutty <roobanshanmugam@gmail.com>
2026-07-25 00:14:00 +0530

    footer logo

--- property-frontend/src/components/Footer.tsx
+++ property-frontend/src/components/Footer.tsx
@@ -2,7 +2,12 @@ export default function Footer() {
   return (
     <footer className="bg-dark text-white mt-5 py-4">
       <div className="container text-center">
-
+        <img
+            className="banner-img"
+            src="/logo.png"
+            alt="LeadTap Property"
+            width="75"
+          />
         <h5>Leadtap Properties</h5>
 
         <p className="mb-1">
