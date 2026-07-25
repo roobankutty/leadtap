317aed37e4ca31088ed6266df0f4be63e029cdca
roobankutty <roobanshanmugam@gmail.com>
2026-07-25 13:05:35 +0530

    route dashboard fix

--- admin-frontend/src/App.tsx
+++ admin-frontend/src/App.tsx
@@ -3,7 +3,7 @@ BrowserRouter,
 Routes,
 Route
 } from "react-router-dom";
-
+import { Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
@@ -35,6 +35,12 @@ return (
   }
 >
 
+<Route
+    path="/"
+    element={
+      <Navigate to="/dashboard" replace />
+    }
+  />
 
 <Route
   path="/dashboard"
