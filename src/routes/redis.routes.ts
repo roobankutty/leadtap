79cb58b4470ba875d8c2c0c4652db7a440598e90
roobankutty <roobanshanmugam@gmail.com>
2026-07-27 00:20:53 +0530

    redis TLS temp log

--- src/routes/redis.routes.ts
+++ src/routes/redis.routes.ts
@@ -4,30 +4,10 @@ import { redisClient } from "../config/redis";
 const router = Router();
 
 router.get("/test", async (req, res) => {
-  try {
-    console.log("isOpen:", redisClient.isOpen);
-    console.log("isReady:", redisClient.isReady);
-
-    await redisClient.ping();
-    console.log("PING OK");
-
-    await redisClient.set("message", "Redis is working!");
-    const value = await redisClient.get("message");
-
-    res.json({
-      success: true,
-      value,
-    });
-  } catch (error: any) {
-    console.error(error);
-
-    res.status(500).json({
-      success: false,
-      message: error.message,
-      name: error.name,
-      code: error.code,
-    });
-  }
+  res.json({
+    isOpen: redisClient.isOpen,
+    isReady: redisClient.isReady,
+  });
 });
 
 export default router;
